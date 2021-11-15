53e3c687f2ec48ead6fa05e62a7955ae02da1dc3
mtchang <michaeltgchang@gmail.com>
2021-11-15 21:10:34 +0800

    remove old code that was trying to access LTA API

--- script.js
+++ script.js
@@ -3,23 +3,15 @@
 // wrapper function to generate the URL for API call by inserting the parameter
 // into the URL string
 // API is LTA DataMall
+// using ArriveLah's proxy server to access LTA data. See https://github.com/cheeaun/arrivelah
 function busArrivalApiCall(busStopCode) {
     return `https://arrivelah2.busrouter.sg/?id=${busStopCode}`;
-    //return `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${busStopCode}`;
 }
-
-// create Header for API
-const myHeaders = new Headers({
-    AccountKey: config.API_ACCOUNT_KEY,
-    accept: "application/json",
-});
 
 // copied from https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript
 // using Fetch API https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 const fetchBusArrivals = async (busStopCode) => {
-    const response = await fetch(busArrivalApiCall(busStopCode), {
-        //headers: myHeaders,
-    });
+    const response = await fetch(busArrivalApiCall(busStopCode));
     const myJson = await response.json(); //extract JSON from the http response
     // do something with myJson
     return myJson;
